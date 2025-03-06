c5214adb4b092bb98556ec8452cf48d77fa8612b
r-likhith <likhithdelulu@gmail.com>
2025-03-06 15:00:28 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,7 @@
-# Flask App Initialization
+from flask import Flask
+from app.routes import configure_routes
+
+def create_app():
+    app = Flask(__name__)
+    configure_routes(app)
+    return app
