4d2e369b2d285a7420c67fd0533a20a5e7097442
r-likhith <likhithdelulu@gmail.com>
2025-03-06 15:53:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)  # Port can be adjusted
+    # Use PORT from environment variable if available (Render provides this), else default to 10000
+    port = int(os.getenv('PORT', 10000))
+
+    # `debug=True` can be removed for production - recommended for local development only
+    app.run(host='0.0.0.0', port=port)
